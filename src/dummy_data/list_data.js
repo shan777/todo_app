a86a85c25661812fe11c90ad174d64f15f737c50
Sarah Han <sarah.s.han@gmail.com>
2018-09-19 15:05:20 -0700

    Completed displaying dummy data to To-do list

---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,28 @@
+
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, milk, bread, and diet coke',
+        _id: '001' //this is how APIs label id with the underscore so add it!
+    },
+    {
+        title: 'Watch React video',
+        details: 'Watch the video 1, 2, and 3 for beginners',
+        _id: '002'
+    },
+    {
+        title: 'Wash the car',
+        details: 'Outside only',
+        _id: '003'
+    },
+    {
+        title: 'Water the plants',
+        details: 'Both front and backyard',
+        _id: '004'
+    },
+    {
+        title: 'Go to gym',
+        details: 'Before end of the year',
+        _id: '005'
+    }       
+];
